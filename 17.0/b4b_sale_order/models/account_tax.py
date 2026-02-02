0898c94861833ef7a0ef158310427e9ec6558705
brawilsolutions <gerente1@brawilsolutions.com>
2026-02-02 11:03:38 -0500

    Remove B4B Sale Order Module
    - Deleted server action for automatic quotation creation.
    - Removed model initialization file and all related models including:
      - B4B Sale Order
      - B4B Sale Order Line
      - Account Tax
      - Product Template
      - Res Config Settings
      - Sale Order
    - Removed associated views and menu items for B4B Sale Order.
    - Cleaned up security access rights for the module.

--- 17.0/b4b_sale_order/models/account_tax.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class AccountTax(models.Model):
-    _inherit = 'account.tax'
-
-    es_iva = fields.Boolean(string='¿Es IVA?', help="Marca este impuesto como IVA para incluirlo en los precios si aplica.")
